f9e0bf62b9e6addf5618825ab9e514e6e058b316
ch <mkmsacc@outlook.com>
2024-01-31 15:55:01 +0700

    Add initial test cases to ExcelUtilsTest class
    An initial set of test cases have been added to the ExcelUtilsTest class. This includes methods for testing key functionalities such as 'getWorkbook', 'closeWorkbook', 'getSheet', 'setCellValue', and 'saveWorkbook'.

---
+++ excel-utils/excel-utils/src/test/java/org/example/ExcelUtilsTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExcelUtilsTest {
+
+    @Test
+    void getWorkbook() {
+    }
+
+    @Test
+    void closeWorkbook() {
+    }
+
+    @Test
+    void getSheet() {
+    }
+
+    @Test
+    void setCellValue() {
+    }
+
+    @Test
+    void saveWorkbook() {
+    }
+}
