7ac33e6427b8db13e5feead778eaf468df5bd070
ch <mkmsacc@outlook.com>
2024-02-01 15:53:59 +0700

    Add test case for ExcelUtils::closeWorkbook method
    The ExcelUtilsTest class is updated with a mock Workbook object for testing. A new test case is added to check if the 'closeWorkbook' method of ExcelUtils class works as expected by verifying whether the 'close' method of the Workbook is called.

--- excel-utils/excel-utils/src/test/java/org/example/ExcelUtilsTest.java
+++ excel-utils/excel-utils/src/test/java/org/example/ExcelUtilsTest.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,6 +17,10 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @ExtendWith(MockitoExtension.class)
 class ExcelUtilsTest {
+
+
+    @Mock
+    private Workbook mockWorkBook;
 
     @Test
     @DisplayName("should throw illegal state exception when initialized")
@@ -30,6 +38,13 @@ class ExcelUtilsTest {
     void shouldCreateWorkbookAsAnInstanceOfXssfWorkbook() {
         var wb = ExcelUtils.createWorkbook();
         assertThat(wb).isInstanceOf(XSSFWorkbook.class);
+    }
+
+    @Test
+    @DisplayName("should close workbook")
+    void shouldCloseWorkbook() throws IOException {
+        ExcelUtils.closeWorkbook(mockWorkBook);
+        Mockito.verify(mockWorkBook).close();
     }
 
 }
