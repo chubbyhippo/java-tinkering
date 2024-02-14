bd3155f8801524c22ee26067c7dc7511f24f9018
ch <mkmsacc@outlook.com>
2024-02-14 20:58:40 +0700

    Add CsvUtils read test and improve imports
    Added a new test method for reading CSV files in CsvUtilsTest. The test assures the correct reading of CSV files. Additionally, cleaned up and organized the import statements to bring in only those which are necessary for the test class.

--- csv-utils/src/test/java/org/example/CsvUtilsTest.java
+++ csv-utils/src/test/java/org/example/CsvUtilsTest.java
@@ -2,14 +2,18 @@ package org.example;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.*;
 
 class CsvUtilsTest {
-   
+
     @Test
     @DisplayName("should throw illegal state exception when initialized")
     void shouldThrowIllegalStateExceptionWhenInitialized() throws NoSuchMethodException {
@@ -19,6 +23,38 @@ class CsvUtilsTest {
         assertThatExceptionOfType(InvocationTargetException.class)
                 .isThrownBy(constructor::newInstance)
                 .withCauseInstanceOf(IllegalAccessException.class);
+    }
+
+    @TempDir
+    private Path tempDir;
+
+    @Test
+    @DisplayName("should read csv")
+    void shouldReadCsv() throws IOException {
+
+        var csv = """
+                name,age
+                foo,10
+                bar,20
+                """;
+
+        var csvPath = tempDir.resolve("test.csv");
+        Files.writeString(csvPath, csv);
+        var records = CsvUtils.readCsv(csvPath);
+
+        assertThat(records).hasSize(3);
+
+        var header = records.getFirst();
+        assertThat(header.get(0)).isEqualTo("name");
+        assertThat(header.get(1)).isEqualTo("age");
+
+        var rec1 = records.get(1);
+        assertThat(rec1.get(0)).isEqualTo("foo");
+        assertThat(rec1.get(1)).isEqualTo("10");
+
+        var rec2 = records.get(2);
+        assertThat(rec2.get(0)).isEqualTo("bar");
+        assertThat(rec2.get(1)).isEqualTo("20");
     }
 
 }
