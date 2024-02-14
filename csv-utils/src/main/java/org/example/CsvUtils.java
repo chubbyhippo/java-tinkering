4b36e3720042e23ece1708d43a42d312899ef648
ch <mkmsacc@outlook.com>
2024-02-14 20:58:50 +0700

    Introduced CSV reading functionality in CsvUtils class
    A method has been added to CsvUtils, to handle reading of CSV data. This leverages Apache Commons CSV for reading operations. Along with this, relevant dependencies are imported to support the reading process.

--- csv-utils/src/main/java/org/example/CsvUtils.java
+++ csv-utils/src/main/java/org/example/CsvUtils.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class CsvUtils {
     private CsvUtils() throws IllegalAccessException {
         throw new IllegalAccessException("Utility Class");
     }
+
+    public static List<CSVRecord> readCsv(Path path) throws IOException {
+        try (Reader reader = Files.newBufferedReader(path);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT)) {
+            return csvParser.getRecords();
+        }
+    }
+
+
 }
