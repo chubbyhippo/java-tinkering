4510bf99e6b6fa4f898c61df86b7fb2dc7fcf7e9
chubbyhippo <mkmsacc@outlook.com>
2024-05-01 17:01:10 +0700

    Add SFTP utilities in SftpUtils class
    Added methods for establishing a client session, uploading, downloading, and deleting files via SFTP in the SftpUtils class. These utilities will make working with SFTP operations more straightforward and reusable across different parts of the project.

--- sftp-utils/src/main/java/org/github/chubbyhippo/SftpUtils.java
+++ sftp-utils/src/main/java/org/github/chubbyhippo/SftpUtils.java
@@ -1,7 +1,56 @@
 package org.github.chubbyhippo;
 
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.sftp.client.SftpClient;
+import org.apache.sshd.sftp.client.SftpClientFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.Files.readAllBytes;
+
 public class SftpUtils {
     private SftpUtils() {
         throw new IllegalStateException("Utility class");
+    }
+
+    private static ClientSession createSession(String username, String host, int port, String password) throws IOException {
+        SshClient client = SshClient.setUpDefaultClient();
+        client.start();
+
+        ClientSession session = client.connect(username, host, port).verify().getSession();
+        session.addPasswordIdentity(password);
+        session.auth().verify();
+
+        return session;
+    }
+
+    public static void upload(String username, String host, int port, String password, Path localFile, String remotePath) throws IOException {
+        try (SshClient client = SshClient.setUpDefaultClient();
+             ClientSession session = createSession(username, host, port, password);
+             SftpClient sftpClient = SftpClientFactory.instance().createSftpClient(session)) {
+
+            sftpClient.write(remotePath).write(readAllBytes(localFile));
+        }
+    }
+
+    public static void download(String username, String host, int port, String password, String remoteFile, Path localPath) throws IOException {
+        try (SshClient client = SshClient.setUpDefaultClient();
+             ClientSession session = createSession(username, host, port, password);
+             SftpClient sftpClient = SftpClientFactory.instance().createSftpClient(session)) {
+
+            copy(sftpClient.read(remoteFile), localPath);
+        }
+    }
+
+    public static void delete(String username, String host, int port, String password, String remoteFile) throws IOException {
+        try (SshClient client = SshClient.setUpDefaultClient();
+             ClientSession session = createSession(username, host, port, password);
+             SftpClient sftpClient = SftpClientFactory.instance().createSftpClient(session)) {
+
+            sftpClient.remove(remoteFile);
+        }
     }
 }
