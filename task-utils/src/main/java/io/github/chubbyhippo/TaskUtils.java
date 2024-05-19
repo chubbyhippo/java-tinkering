ca6cce76450c15a382ea11330f4ad9155d3fd6e0
chubbyhippo <mkmsacc@outlook.com>
2024-05-19 09:34:14 +0700

    Add method to run and monitor CompletableFuture
    Added a new static utility method 'runCompletableFutureVoid' into 'TaskUtils' class to execute a CompletableFuture and monitor its execution time. The method takes a Runnable, timeout value and TimeUnit as parameters and throws a TimeoutException if the task exceeds the given timeout.

--- task-utils/src/main/java/io/github/chubbyhippo/TaskUtils.java
+++ task-utils/src/main/java/io/github/chubbyhippo/TaskUtils.java
@@ -1,7 +1,28 @@
 package io.github.chubbyhippo;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 public class TaskUtils {
     private TaskUtils() {
         throw new IllegalStateException("Utility class");
+    }
+    public static void runCompletableFutureVoid(Runnable runnable, long timeout, TimeUnit timeUnit) throws TimeoutException {
+        CompletableFuture<Void> future = CompletableFuture.runAsync(runnable);
+        try {
+            future.get(timeout, timeUnit);
+            System.out.println("Current thread :" + Thread.currentThread().getName());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            System.out.println("The task exceeded the given timeout of: " + timeout + " " + timeUnit);
+            System.out.println("Current thread :" + Thread.currentThread().getName());
+            future.cancel(true);
+            System.out.println("Current thread :" + Thread.currentThread().getName());
+            e.printStackTrace();
+            throw new TimeoutException("Timed out");
+        }
     }
 }
