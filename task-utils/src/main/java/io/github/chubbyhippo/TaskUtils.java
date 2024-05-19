ea03713cc4af8535a32e984d36919f74297f612e
chubbyhippo <mkmsacc@outlook.com>
2024-05-19 16:02:14 +0700

    Refactor TaskUtils and add support for Callable task
    The TaskUtils class has been refactored to enhance readability and to better follow good programming principles. Specifically, the runTaskExecutor method has been moved for improved organization. Additionally, a new runTaskExecutor method has been added that supports Callable tasks, returning an Optional result. This provides added flexibility for handling tasks that return a result.

--- task-utils/src/main/java/io/github/chubbyhippo/TaskUtils.java
+++ task-utils/src/main/java/io/github/chubbyhippo/TaskUtils.java
@@ -1,5 +1,6 @@
 package io.github.chubbyhippo;
 
+import java.util.Optional;
 import java.util.concurrent.*;
 
 public class TaskUtils {
@@ -10,15 +11,6 @@ public class TaskUtils {
     public static void runCompletableFutureVoid(Runnable runnable, long timeout, TimeUnit timeUnit) throws TimeoutException {
         CompletableFuture<Void> future = CompletableFuture.runAsync(runnable);
         executeRunnable(timeout, timeUnit, future);
-    }
-
-    public static void runTaskExecutor(Runnable runnable, long timeout, TimeUnit timeUnit) throws TimeoutException {
-
-        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
-            Future<?> future = executorService.submit(runnable);
-            executeRunnable(timeout, timeUnit, future);
-        }
-
     }
 
     private static void executeRunnable(long timeout, TimeUnit timeUnit, Future<?> future) throws TimeoutException {
@@ -36,4 +28,30 @@ public class TaskUtils {
             throw new TimeoutException("Timed out");
         }
     }
+
+    public static void runTaskExecutor(Runnable runnable, long timeout, TimeUnit timeUnit) throws TimeoutException {
+
+        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
+            Future<?> future = executorService.submit(runnable);
+            executeRunnable(timeout, timeUnit, future);
+        }
+    }
+
+    public static <T> Optional<T> runTaskExecutor(Callable<T> task, long timeout, TimeUnit timeUnit) throws TimeoutException {
+        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+            Future<T> future = executorService.submit(task);
+            try {
+                return Optional.ofNullable(future.get(timeout, timeUnit));
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                future.cancel(true);
+                System.out.println("The task exceeded the given timeout of: " + timeout + " " + timeUnit);
+                throw new TimeoutException("Timed out");
+            }
+        }
+        return Optional.empty();
+    }
+
+
 }
