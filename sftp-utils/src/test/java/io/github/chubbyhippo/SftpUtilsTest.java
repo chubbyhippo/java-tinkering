2e8b2c80b18ce28bc443e4a0da58ddb783c1c543
chubbyhippo <mkmsacc@outlook.com>
2024-08-31 22:03:43 +0700

    Add SFTP server setup and new test for session connection
    This commit sets up an in-memory SFTP server for testing purposes and includes a new test to verify that a session can be successfully established. This enhances the testing suite by allowing more comprehensive integration tests for SFTP functionalities.

--- sftp-utils/src/test/java/io/github/chubbyhippo/SftpUtilsTest.java
+++ sftp-utils/src/test/java/io/github/chubbyhippo/SftpUtilsTest.java
@@ -1,13 +1,28 @@
 package io.github.chubbyhippo;
 
+import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Path;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SftpUtilsTest {
+
+    private static SshServer sshServer;
+    private static final int PORT = 22;
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+
     @Test
     @DisplayName("should throw illegal state exception when initialized")
     void shouldThrowIllegalStateExceptionWhenInitialized() {
@@ -21,5 +36,32 @@ class SftpUtilsTest {
             assertThat(exception.getCause().getMessage()).isEqualTo("Utility class");
         }
     }
+
+    @BeforeAll
+    public static void setUp() throws IOException {
+        sshServer = SshServer.setUpDefaultServer();
+        sshServer.setPort(PORT);
+        sshServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+        sshServer.setPasswordAuthenticator((username, password, session) -> true);
+        sshServer.setSubsystemFactories(List.of(new SftpSubsystemFactory()));
+        sshServer.setFileSystemFactory(new VirtualFileSystemFactory(Path.of("target/sftp-root")));
+        sshServer.start();
+    }
+
+    @AfterAll
+    public static void tearDown() throws IOException {
+        if (sshServer != null && sshServer.isStarted()) {
+            sshServer.stop();
+        }
+    }
+
+    @Test
+    @DisplayName("should return session when connecting to sftp server")
+    void shouldReturnSessionWhenConnectingToSftpServer() throws IOException {
+        try (var session = SftpUtils.createSession(USERNAME, "localhost", PORT, PASSWORD)) {
+            assertThat(session.isOpen()).isTrue();
+        }
+    }
+
 
 }
