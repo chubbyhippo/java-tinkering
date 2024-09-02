4a82b5cb8553069999cb30773fe31c1111bd3727
ch <mkmsacc@outlook.com>
2024-09-02 20:28:44 +0700

    Set up and tear down embedded SFTP server for tests
    Added an embedded SFTP server using Apache SSHD to the JschClientTest class for better isolation and consistency in tests. Implemented methods to set up and tear down the server before and after all tests, respectively. Updated existing tests to utilize the embedded server configuration.

--- sftp-utils/src/test/java/io/github/chubbyhippo/JschClientTest.java
+++ sftp-utils/src/test/java/io/github/chubbyhippo/JschClientTest.java
@@ -2,30 +2,67 @@ package io.github.chubbyhippo;
 
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
+import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JschClientTest {
 
-    private final String username = "foo";
-    private final String password = "pass";
-    private final String host = "localhost";
-    private final int port = 2222;
+    private static final int PORT = 22;
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private final String HOST = "localhost";
 
+    @TempDir
+    private static Path tempDir;
+
+    private static SshServer sshServer;
+
+    @BeforeAll
+    static void setUp() throws IOException {
+        var file = tempDir.resolve("test.txt");
+        Files.write(file, "test".getBytes());
+        sshServer = SshServer.setUpDefaultServer();
+        sshServer.setPort(PORT);
+        sshServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+        sshServer.setPasswordAuthenticator((username, password, session) -> true);
+        sshServer.setSubsystemFactories(List.of(new SftpSubsystemFactory()));
+        sshServer.setFileSystemFactory(new VirtualFileSystemFactory(tempDir));
+        sshServer.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        if (sshServer != null && sshServer.isStarted()) {
+            sshServer.stop();
+        }
+    }
 
     @Test
     @DisplayName("should list file from sftp server")
     void shouldListFileFromSftpServer() throws JSchException, SftpException {
         var jschClient = JschClient.create()
-                .withCredentials(username, password, host, port)
+                .withCredentials(USERNAME, PASSWORD, HOST, PORT)
                 .withDefaultConfigs()
                 .connect();
 
-        var remoteDir = "/upload";
+        var remoteDir = "/";
         var filenames = jschClient.listFiles(remoteDir);
 
         assertThat(filenames).isNotEmpty();
     }
+
 }
